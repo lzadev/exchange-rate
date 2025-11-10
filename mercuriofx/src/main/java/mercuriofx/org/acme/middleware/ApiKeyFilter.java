d2ebfb1b9cbf8b6f24a0e7bebdc88feb557d7aa7
Luis Zabala <ldelorden@mardom.com>
2025-11-10 15:46:36 -0400

    feat: implement API key authentication filter; add ApiKeyFilter class for request validation and logging

---
+++ mercuriofx/src/main/java/mercuriofx/org/acme/middleware/ApiKeyFilter.java
@@ -0,0 +1,44 @@
+package mercuriofx.org.acme.middleware;
+
+import java.io.IOException;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import mercuriofx.org.acme.dto.ErrorResponse;
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class ApiKeyFilter implements ContainerRequestFilter {
+    @ConfigProperty(name = "api_key")
+    String apiKey;
+
+    private static final Logger LOG = Logger.getLogger(ApiKeyFilter.class);
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        final var providedApiKey = requestContext.getHeaderString("x-api-key");
+
+        if (apiKey == null || !apiKey.equals(providedApiKey)) {
+            LOG.warnf("Unauthorized access attempt with API key: %s", providedApiKey);
+
+            final var errorResponse = new ErrorResponse("Invalid or missing API key");
+
+            requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED)
+                        .entity(errorResponse   )
+                        .type(MediaType.APPLICATION_XML)
+                        .build()
+            );
+        }
+
+        LOG.infof("Authorized request");
+    }
+}
