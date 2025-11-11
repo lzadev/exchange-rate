3043b679d94e63de3596744cdb3901303aacf0a4
Luis Zabala <ldelorden@mardom.com>
2025-11-10 21:51:33 -0400

    feat: update provider URLs for exchange rates; add BadRequestException class for validation and enhance Docker configuration

---
+++ Gateway/Gateway.Application/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,3 @@
+namespace Gateway.Application.Common.Exceptions;
+
+public class BadRequestException(string message): Exception(message);
--- Gateway/Gateway.Application/Exchanges/ExchangeService.cs
+++ Gateway/Gateway.Application/Exchanges/ExchangeService.cs
@@ -10,6 +10,8 @@ internal class ExchangeService(
 {
     public async Task<ExchangeResponse> GetExchangeAsync(ExchangeQuery query)
     {
+        Validate(query);
+        
         var result = await Task.WhenAll(exchangeProviders.Select(x=>
             x.GetRateAsync(query.From, query.To, query.Amount)).ToArray());
         
@@ -19,5 +21,20 @@ internal class ExchangeService(
 
         return validResults.Length == 0 ? throw new NotResultException("No exchange rates available from providers") 
             : new ExchangeResponse(validResults[0]);
+    }
+    
+    private static void Validate(ExchangeQuery query)
+    {
+        if (string.IsNullOrWhiteSpace(query.From))
+            throw new BadRequestException("From currency is required.");
+        
+        if (string.IsNullOrWhiteSpace(query.To))
+            throw new BadRequestException("To currency is required.");
+        
+        if (query.Amount <= 0)
+            throw new BadRequestException("Amount must be greater than zero.");
+        
+        if(query.From.ToUpper() == query.To.ToUpper())
+            throw new BadRequestException("From and To currencies must be different.");
     }
 }
